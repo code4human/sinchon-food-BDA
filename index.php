c341ee8ac4176c7aa5097d69638f65e730136b7d
TenaKim <tena.kim10@gmail.com>
2020-11-12 02:39:01 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8">
+<title>Sinchon Food BDA</title>
+</head>
+<body> 
+	<header>
+    	<?php include "header.php";?>
+    </header>
+	<section>
+	    <?php include "main.php";?>
+	</section> 
+	<footer>
+    	<?php include "footer.php";?>
+    </footer>
+</body>
+</html>
