8afd56211058565635264e28e5e03bc26f3bcd0b
TenaKim <tena.kim10@gmail.com>
2020-11-15 01:26:34 +0900

    Create main.php and style

---
+++ php/main.php
@@ -0,0 +1,11 @@
+        <div id="main_img" style="height: 200px; text-align: center; background-color: #FFE4B5;">
+            <img src="../../img/banner.png" style="float: center; max-height: 200px; width: auto;">
+        </div>
+        <div id="main_content">
+            <div id="best_food">
+                <h4>Registered Best Restaurants</h4>
+            </div>
+            <div id="best_custom">
+                <h4>Your Taste</h4>
+            </div>
+        </div>
